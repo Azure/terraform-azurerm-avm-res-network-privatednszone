d4f9cb56ee9a4c971ae6b38494aad6d4faf7aa07
chiancw <chiancw@gmail.com>
2024-02-19 13:12:04 +0800

    updated formatting for examples output.tf

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,45 +1,45 @@
 output "private_dns_zone_output" {
-  value = module.private_dns_zones.private_dnz_zone_output
+  value       = module.private_dns_zones.private_dnz_zone_output
   description = "The private dns zone output"
 }
 
 
 output "a_record_outputs" {
-  value = module.private_dns_zones.a_record_outputs
+  value       = module.private_dns_zones.a_record_outputs
   description = "The a record output"
 }
 
 output "aaaa_record_outputs" {
-  value = module.private_dns_zones.aaaa_record_outputs
+  value       = module.private_dns_zones.aaaa_record_outputs
   description = "The aaaa record output"
 }
 
 output "virtual_network_link_outputs" {
-  value = module.private_dns_zones.virtual_network_link_outputs
+  value       = module.private_dns_zones.virtual_network_link_outputs
   description = "The virtual network link output"
 }
 
 output "cname_record_outputs" {
-  value = module.private_dns_zones.cname_record_outputs
+  value       = module.private_dns_zones.cname_record_outputs
   description = "The cname record output"
 }
 
 output "mx_record_outputs" {
-  value = module.private_dns_zones.mx_record_outputs
+  value       = module.private_dns_zones.mx_record_outputs
   description = "The mx record output"
 }
 
 output "ptr_record_outputs" {
-  value = module.private_dns_zones.ptr_record_outputs
+  value       = module.private_dns_zones.ptr_record_outputs
   description = "The ptr record output"
 }
 
 output "srv_record_outputs" {
-  value = module.private_dns_zones.srv_record_outputs
+  value       = module.private_dns_zones.srv_record_outputs
   description = "The srv record output"
 }
 
 output "txt_record_outputs" {
-  value = module.private_dns_zones.txt_record_outputs
+  value       = module.private_dns_zones.txt_record_outputs
   description = "The txt record output"
 }
