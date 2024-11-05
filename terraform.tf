b318ba44658264fea1a2e21398db3e415cad3196
dumontj <james.dumont-ext@amundi.com>
2024-11-05 08:42:22 +0000

    Adjust the azurerm minimum version to 4.0 and valide avm pr-check

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.116.0, < 5.0"
+      version = ">= 4.0, < 5.0"
     }
     modtm = {
       source  = "azure/modtm"
