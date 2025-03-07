a661f5d00431b5ac87b0f91806f304e5be80ebd3
chian <91816369+chianw@users.noreply.github.com>
2025-03-07 06:20:23 +0000

    updated to remove azuread provider at root module

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.40"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.0, < 5.0"
