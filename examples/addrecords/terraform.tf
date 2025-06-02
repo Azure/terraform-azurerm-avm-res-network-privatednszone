648485808763af63c6ba1c7ed85856bd912700c2
Remo Leone Laudo <remoleone@icloud.com>
2025-06-02 11:58:06 -0400

    Ran avm pre-commit

--- examples/addrecords/terraform.tf
+++ examples/addrecords/terraform.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.0, < 5.0"
-    }
     azapi = {
       source  = "Azure/azapi"
       version = "~> 2.2"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0, < 5.0"
     }
   }
 }
--- examples/default/terraform.tf
+++ examples/default/terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.2"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = ">= 2.40"
@@ -8,10 +12,6 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.0, < 5.0"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.2"
     }
   }
 }
