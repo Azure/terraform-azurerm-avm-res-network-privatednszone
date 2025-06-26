13f4090ee71d4193ed382f6dcf841e2aad4d570a
Remo Leone Laudo <remoleone@icloud.com>
2025-06-26 08:57:00 -0400

    Added validation for virtual network link name length

--- variables.tf
+++ variables.tf
@@ -137,7 +137,7 @@ variable "role_assignments" {
   default     = {}
   description = <<DESCRIPTION
   A map of role assignments to create on the <RESOURCE>. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
-  
+
   - `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
   - `principal_id` - The ID of the principal to assign the role to.
   - `description` - (Optional) The description of the role assignment.
@@ -146,7 +146,7 @@ variable "role_assignments" {
   - `condition_version` - (Optional) The version of the condition syntax. Leave as `null` if you are not using a condition, if you are then valid values are '2.0'.
   - `delegated_managed_identity_resource_id` - (Optional) The delegated Azure Resource Id which contains a Managed Identity. Changing this forces a new resource to be created. This field is only used in cross-tenant scenario.
   - `principal_type` - (Optional) The type of the `principal_id`. Possible values are `User`, `Group` and `ServicePrincipal`. It is necessary to explicitly set this attribute when creating role assignments if the principal creating the assignment is constrained by ABAC rules that filters on the PrincipalType attribute.
-  
+
   > Note: only set `skip_service_principal_aad_check` to true if you are assigning a role to a service principal.
   DESCRIPTION
   nullable    = false
@@ -278,5 +278,9 @@ variable "virtual_network_links" {
       ((v.virtual_network_id != null) && length(v.virtual_network_id) > 0)
     ])
     error_message = "Each virtual_network_link must have either vnetid or virtual_network_id provided."
+  }
+  validation {
+    condition     = alltrue([for link in var.virtual_network_links : length(try(link.name, link.vnetlinkname)) < 80])
+    error_message = "Each virtual network link name must have less than 80 characters."
   }
 }
