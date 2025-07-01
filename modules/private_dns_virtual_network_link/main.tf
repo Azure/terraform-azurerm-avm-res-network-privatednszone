24ff14591e98565f28de1dc3b2053ad63ea56ba4
Remo Leone Laudo <remoleone@icloud.com>
2025-07-01 16:01:36 -0400

    Added second block that handles private link zones and custom dns zones

--- modules/private_dns_virtual_network_link/main.tf
+++ modules/private_dns_virtual_network_link/main.tf
@@ -1,9 +1,51 @@
-resource "azapi_resource" "private_dns_zone_network_link" {
+resource "azapi_resource" "private_link_zone_network_link" {
+  count = var.private_dns_zone_supports_private_link ? 1 : 0
+
   location  = "global"
   name      = var.name
   parent_id = var.parent_id
   type      = "Microsoft.Network/privateDnsZones/virtualNetworkLinks@2024-06-01"
-  body      = var.private_dns_zone_supports_private_link ? local.private_link_private_dns_zone_body : local.custom_private_dns_zone_body
+  body = {
+    properties = {
+      registrationEnabled = var.registration_enabled
+      resolutionPolicy    = var.resolution_policy
+      virtualNetwork = {
+        id = var.virtual_network_id
+      }
+    }
+  }
+  response_export_values = {
+    "id"                   = "id"
+    "name"                 = "name"
+    "type"                 = "type"
+    "virtual_network"      = "properties.virtualNetwork"
+    "registration_enabled" = "properties.registrationEnabled"
+  }
+  tags = var.tags
+
+  timeouts {
+    create = var.timeouts.create
+    delete = var.timeouts.delete
+    read   = var.timeouts.read
+    update = var.timeouts.update
+  }
+}
+
+resource "azapi_resource" "private_dns_zone_network_link" {
+  count = !var.private_dns_zone_supports_private_link ? 1 : 0
+
+  location  = "global"
+  name      = var.name
+  parent_id = var.parent_id
+  type      = "Microsoft.Network/privateDnsZones/virtualNetworkLinks@2024-06-01"
+  body = {
+    properties = {
+      registrationEnabled = var.registration_enabled
+      virtualNetwork = {
+        id = var.virtual_network_id
+      }
+    }
+  }
   response_export_values = {
     "id"                   = "id"
     "name"                 = "name"
--- modules/private_dns_virtual_network_link/outputs.tf
+++ modules/private_dns_virtual_network_link/outputs.tf
@@ -1,9 +1,9 @@
 output "resource" {
   description = "The outputs of the virtual network link resource."
-  value       = azapi_resource.private_dns_zone_network_link.output
+  value       = var.private_dns_zone_supports_private_link ? azapi_resource.private_link_zone_network_link[0].output : azapi_resource.private_dns_zone_network_link[0].output
 }
 
 output "resource_id" {
   description = "The resource ID of the created virtual network link."
-  value       = azapi_resource.private_dns_zone_network_link.id
+  value       = var.private_dns_zone_supports_private_link ? azapi_resource.private_link_zone_network_link[0].id : azapi_resource.private_dns_zone_network_link[0].id
 }
