a9158ec0574dce3a57e5516489fdf8db54658313
Min Hein Aung <minheinaung97@gmail.com>
2026-01-08 03:27:39 +0000

    add azurerm provider version constraints

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "Azure/azapi"
       version = "~> 2.4"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4, < 5"
+    }
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
